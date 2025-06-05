b1eff348d8d2b63f2ff9f5401385cb60e2eb9b01
Jam-Music-Center <admin@jammusiccenter.com>
2025-06-05 15:35:30 +0530

    Initial commit with websit code

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const products = [
     price: "₹7,500",
     stock: "12 in stock",
     description: "Great beginner-friendly guitar with a warm tone.",
-    image: "https://www.bing.com/images/search?view=detailV2&ccid=nsSMfsQZ&id=9AB60113282412E6FF145CCB9BF3DCE4A2A155AE&thid=OIP.nsSMfsQZddGDVgXgb7xuxQHaLH&mediaurl=https%3a%2f%2fdn9ml16rd2hb8.cloudfront.net%2f2019%2f09%2f187-3972-Fender-Acoustic-Guitar_MG_2913.jpg&exph=4104&expw=2736&q=acoutstic+guitar&simid=608024128753390631&FORM=IRPRST&ck=9C2B4A4A400465DE4C16A8ACDFC81BB1&selectedIndex=0&itb=0"
+    image: "https://dn9ml16rd2hb8.cloudfront.net/2019/09/187-3972-Fender-Acoustic-Guitar_MG_2913.jpg"
   },
   {
     name: "Digital Keyboard",
@@ -15,7 +15,7 @@ const products = [
     price: "₹8,200",
     stock: "5 in stock",
     description: "61-key keyboard with excellent features for learners.",
-    image: "https://www.bing.com/images/search?view=detailV2&ccid=Sbmkyt36&id=3901C244C461525B6ABC7BA61F21693712F4771C&thid=OIP.Sbmkyt36aRZ7X1Z_Gqom-wHaHa&mediaurl=https%3a%2f%2fcdn.shopify.com%2fs%2ffiles%2f1%2f0202%2f0250%2fproducts%2fDGX660B-angle-top.jpg%3fv%3d1478633496&exph=2048&expw=2048&q=digital+keyboard&simid=608026800249578034&FORM=IRPRST&ck=73527AB8970B7E4ECF24C11A47D4C8E4&selectedIndex=1&itb=0"
+    image: "https://cdn.shopify.com/s/files/1/0202/0250/products/DGX660B-angle-top.jpg?v=1478633496"
   },
   {
     name: "Electronic Drum Kit",
@@ -24,7 +24,7 @@ const products = [
     price: "₹29,000",
     stock: "3 in stock",
     description: "Compact, mesh-head kit with great sensitivity and feel.",
-    image: "https://www.bing.com/images/search?view=detailV2&ccid=fTHt%2bejB&id=4DA192D98DB5A55A8F9146BDFF33C7BA18FB4C5A&thid=OIP.fTHt-ejBdfo1fG-YSZ_E2AHaFw&mediaurl=https%3a%2f%2fmedia.sweetwater.com%2fimages%2fitems%2f1800%2fTD27KVSet-xlarge.jpg%3fv%3de28ee586171fc1d8&exph=1398&expw=1800&q=electronic+drum+kit&simid=607998706844321381&FORM=IRPRST&ck=A7B948AB92AE09D289E66CE370C407AC&selectedIndex=2&itb=0"
+    image: "https://media.sweetwater.com/images/items/1800/TD27KVSet-xlarge.jpg?v=e28ee586171fc1d8"
   }
 ];
 
